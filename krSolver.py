36bb8d62b7b0900f93700667c5d5616c01b0d983
Andrew Chen <achen88@mit.edu>
2018-01-17 22:52:30 +0100

    what should be a working solution

--- krSolver.py
+++ krSolver.py
@@ -18,33 +18,59 @@ def generate_captcha(text):
 	#draw text
 	draw = ImageDraw.Draw(pil_img)
 	font = ImageFont.truetype("/Library/Fonts/Microsoft/Verdana.ttf", 37)
+	size = draw.textsize(text, font)
+	print(size)
 	draw.text((10, 3), text, font=font, fill=(0))
 
-	return np.array(pil_img)
+	return threshold(np.array(pil_img), .6), size
 
 def threshold(grayscale, threshold):
 	th, out = cv2.threshold(grayscale, threshold * 255, 255, cv2.THRESH_BINARY)
 	return out
 
 if __name__ == '__main__':
-	filename = 'kingsreward_002.jpeg'
+	filename = '../kingsreward_001.jpeg'
 	alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-	min_score = 10e10
-	guess = ''
+	guess = 'xKQsdfsdf'
 	best_ch = ''
 
 	kr = threshold(cv2.imread(filename, cv2.IMREAD_GRAYSCALE), .6)
 
+	# sc1, im1 = score(kr, generate_captcha('xKQ'))
+	# sc2, im2 = score(kr, generate_captcha('xKQj'))
+	# print sc1
+	# print sc2
+	# cv2.imshow("progress1", im1)
+	# cv2.imshow("progress2", im2)
+	# cv2.waitKey(0)
+
 	while len(guess) < 5:
+		base_score = score(kr, generate_captcha(guess))[0]
+		min_score = 10e10
+		guesses = []
 		for ch in alphabet:
-			if score(kr, generate_captcha(guess + ch))[0] < min_score:
-				min_score = score(kr, generate_captcha(guess + ch))[0]
+			ch_score, diff = score(generate_captcha(ch), generate_captcha(''))
+			# print(ch_score)
+			# cv2.imshow("progress", diff)
+			# cv2.waitKey(0)
+			
+			guess_score = score(kr, generate_captcha(guess + ch))[0]
+
+			gain = base_score - guess_score
+			guesses.append(((ch_score - gain), gain, ch, ch_score))
+
+			if (ch_score - gain) < min_score:
+				min_score = (ch_score - gain)
 				best_ch = ch
+
 		guess += best_ch
+		guesses.sort()
+		for line in guesses:
+			print(line)
 
 		cv2.imshow("progress", score(kr, generate_captcha(guess))[1])
 		cv2.waitKey(0)
 
-		print min_score
+		#print min_score
 
-	print guess
+	print(guess)
